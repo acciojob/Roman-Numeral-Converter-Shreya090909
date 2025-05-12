434ed5fa5542e2b5ddee9a568e105ea56ce421fc
Shreya090909 <2205103110011@paruluniversity.ac.in>
2025-05-12 13:29:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 function convertToRoman(num) {
   	const obj = {
       0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+	  1:['CM',900],
+      2:['D', 500], 
+      3:['CD', 400], 
+      4:['C', 100], 
+      5:['XC', 90], 
+      6:['L', 50], 
+      7:['XL', 40],
+	  8:['X', 10],
+      9:['IX', 9],
+	  10:['V', 5],
+      11:['IV', 4],
+	  12:['I', 1],
+    }
 
-  let a='36'
+  let n=798;
+	let ans=""
+	while (n!=0) 
+	{
+		for (let i in romanmap) 
+			
+		}
+	let value=romanmap[i][0];
+	let num=romanmap[i][1]
+	if(num<=n)
+	{
+		ans=ans+value;
+		n=n-num;
+		break;
+	}
+	}
 
 }
  You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line

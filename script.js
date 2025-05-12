932f4471866a1c080d0686de62902b1b05475342
Shreya090909 <2205103110011@paruluniversity.ac.in>
2025-05-12 10:23:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+  let a='36'
 
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+ You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(a));
 
 
 
